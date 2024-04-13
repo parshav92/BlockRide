dbe8c2cb59a36086f436673d8d8bba425ffadc40
parshav92 <dedhiaparshav@gmail.com>
2024-04-14 04:46:12 +0530

    Add Stepper component and update imports and paths in jsconfig.json

--- trafficpages/trafficnremain/src/app/page.js
+++ trafficpages/trafficnremain/src/app/page.js
@@ -1,16 +1,18 @@
+import { Stepper } from "@mui/material";
 import IntWallet from "../components/IntWallet";
 import Traffictoast from "../components/Traffictoast";
 // import Image from "next/image";
 import ProfileDropdown from "@/components/ProfileDropdown";
+import VerticalLinearStepper from "../components/Stepper";
 
 export default function Home() {
   return (
     <>
       <div>
+        {/* <ProfileDropdown /> */}
         {/* <Traffictoast /> */}
-
+        <VerticalLinearStepper />
         <IntWallet />
-        <ProfileDropdown />
       </div>
     </>
   );
