f799e3a6e377108b051cba10c7da15c2a6c38ab0
parshav92 <dedhiaparshav@gmail.com>
2024-04-14 06:14:30 +0530

    hero

--- trafficpages/trafficnremain/src/app/page.js
+++ trafficpages/trafficnremain/src/app/page.js
@@ -2,17 +2,19 @@ import { Stepper } from "@mui/material";
 import IntWallet from "../components/IntWallet";
 import Traffictoast from "../components/Traffictoast";
 // import Image from "next/image";
-import ProfileDropdown from "@/components/ProfileDropdown";
+import ProfileDropdown from "../components/ProfileDropdown";
 import VerticalLinearStepper from "../components/Stepper";
+import { Hero } from "../components/ui/hero";
 
 export default function Home() {
   return (
     <>
       <div>
+        <Hero />
         {/* <ProfileDropdown /> */}
         {/* <Traffictoast /> */}
-        <VerticalLinearStepper />
-        <IntWallet />
+        {/* <VerticalLinearStepper />
+        <IntWallet /> */}
       </div>
     </>
   );
